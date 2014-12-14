207b57c0b3c97f6d1a2aa4f8124d6dd6d5ed3dda
thechrisjordan <cljordan1@gmail.com>
2014-12-14 17:45:40 -0500

    Start rest endpoints

--- server.js
+++ server.js
@@ -1,82 +1,46 @@
-var appRoot = __dirname,
-    express = require('express'),
-    mongoose = require('mongoose'),
-    mAdmin = mongoose.mongo.Admin,
-    app = express(),
-    port = '3001';
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var Bear       = require('./model');
+var port = 3001;
+var router = express.Router();
 
-mongoose.connect('mongodb://localhost:27017/blah');
+mongoose.connect('mongodb://localhost/rest');
 
-var kittySchema = mongoose.Schema({
-    name: String
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//*** Start Middleware
+
+// Logs a message when something is happening with the api
+router.use(function (req, res, next) {
+    console.log('Something is happening');
+    next();
 });
 
-kittySchema.methods.speak = function () {
-    var greeting = this.name
-        ? "Meow name is " + this.name
-        : "I don't have a name"
-    console.log(greeting);
-};
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-
-var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak() // "Meow name is fluffy"
-
-fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
+router.get('/', function (req, res) {
+    res.json({message: 'welcome to the api'});
 });
 
+router.route('/bears')
+    .post(function (req, res) {
+        var model = new Bear();
 
-var buffy = new Kitten({ name: 'buffy' });
-buffy.speak() // "Meow name is fluffy"
+        model.name = req.body.name; // name comes from the request
 
-buffy.save(function (err, buffy) {
-    if (err) return console.error(err);
-    buffy.speak();
-});
+        model.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
 
-
-Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-});
-
-//Kitten.find({ name: /^Fluff/ }, callback);
+            res.json({message: 'Model Item Created!'});
+        });
+    });
 
 
-var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name) // 'Silence'
 
-//var connection = mongoose.createConnection(
-//    'mongodb://localhost:27017/blah');
-
-console.log(mongoose.connection.readyState);
-var db = mongoose.connection;
-
-// check mongoose connectivity
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-    // yay!
-    console.log(mongoose.connection.readyState);
-
-    //new mAdmin(connection.db).listDatabases(function(err, result) {
-    //    console.log('listDatabases succeeded');
-    //    // database list stored in result.databases
-    //    var allDatabases = result;
-    //    console.log(allDatabases);
-    //});
-
-});
-
-app.get('/', function (req, res) {
-    //console.log(req);
-    console.log('Node started at localhost:' + port);
-    res.send('hello earthling');
-});
-
-
+app.use('/api', router);
 
 app.listen(port);
+console.log('app started on port ' + port);
